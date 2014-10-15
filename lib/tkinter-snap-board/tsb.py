a28093236e531b3945a710844bb03821061fa34d
Bryan Gorges <bgorges@jach.hawaii.edu>
2014-10-15 09:54:51 -1000

    Resolved #2. Fixed token problem. Tested with multiple windows.

--- lib/tkinter-snap-board/tsb.py
+++ lib/tkinter-snap-board/tsb.py
@@ -8,21 +8,23 @@ class TSB(Frame):
 		self.m = master
 		self.f = Frame.__init__(self, master)
 		self.h = spacing
-		self.w = spacing * 1.33
+		self.w = int(spacing * 1.33)
 		self.size = size
 		self.buff = buff
 		self.canvas = Canvas(self.f, width=self.w*size,
 												 height=self.h*size)
 		self.canvas.pack(fill="both", expand=True)
 		self._drag_data = {"x": 0, "y": 0, "grab": None}
-		self._create_token((self.w, self.h), "white")
-		self._create_token((self.w*2, self.h*2), "black")
+		self._create_token("white", cell=[1,1])
+		self._create_token("black", cell=[2,2])
 		self.canvas.tag_bind("token", "<ButtonPress-1>", self.OnTokenButtonPress)
 		self.canvas.tag_bind("token", "<ButtonRelease-1>", self.OnTokenButtonRelease)
 		self.canvas.tag_bind("token", "<B1-Motion>", self.OnTokenMotion)
 		self.show_grid()
 		self.testframe = Frame(self.f, bd=1, relief=RAISED)
+		self.testframe2 = Frame(self.f, bd=1, relief=RAISED)
 		self.add_window(self.testframe, cell=[1,1])
+		self.add_window(self.testframe2, cell=[2,2])
 		#self.remove_grid()
 
 	def snap_grab(self, mx, my):
@@ -38,9 +40,10 @@ class TSB(Frame):
 		"""Snaps window into cell following its grab"""
 		if self._drag_data["window"] is not None:
 			coords = self.canvas.coords(self._drag_data["grab"])
-			newx = coords[0]
-			newy = coords[1] + self.buff
-			self.canvas.coords(self._drag_data["window"], newx, newy)
+			newx = coords[0] + self.w / 2
+			newy = coords[1] + (self.buff + self.h) / 2
+			self.canvas.coords(self._drag_data["window"],
+							   newx, newy)
 
 	def calculate_snap(self, mouse=None, cell=None):
 		if cell is not None:
@@ -57,8 +60,8 @@ class TSB(Frame):
 			for y in range(self.size+1):
 				dy = y * self.h
 				self.canvas.create_oval(dx-2, dy-2, dx+2, dy+2,
-																outline="black", fill="black",
-																tags="grid")
+										outline="black", fill="black",
+										tags="grid")
 
 	def remove_grid(self):
 		self.canvas.delete("grid")
@@ -70,15 +73,16 @@ class TSB(Frame):
 		newy = newy + self.buff + (newh / 2) #to show grab
 		newx = newx + (self.w / 2)
 		self.canvas.create_window(newx, newy, window=window,
-															height=newh, width=self.w,
-															tags="window")
+								  height=newh, width=self.w,
+								  tags="window")
 
-	def _create_token(self, coord, color):
+	def _create_token(self, color, cell=None, mouse=None):
 		'''Create a token at the given coordinate in the given color'''
-		(x,y) = coord
+		x, y = self.calculate_snap(cell=cell, mouse=mouse)
+		print x, y
 		self.canvas.create_rectangle(x, y, x+self.w, y+self.h,
-																 outline=color, fill=color,
-																 tags="token")
+									 outline=color, fill=color,
+									 tags="token")
 
 	def OnTokenButtonPress(self, event):
 		'''Being drag of an object'''
@@ -86,12 +90,14 @@ class TSB(Frame):
 		self._drag_data["grab"] = self.canvas.find_closest(event.x, event.y)[0]
 		self._drag_data["orig_coords"] = self.canvas.coords(self._drag_data["grab"])
 		x1, y1, x2, y2 = tuple(self._drag_data["orig_coords"])
+		print self.canvas.find_enclosed(x1-1, y1-1, x2+1, y2+1)
 		try:
-			self._drag_data["window"] = self.canvas.find_enclosed(x1-1, y1-1, x2+1, y2+1)[0]
+			self._drag_data["window"] = self.canvas.find_enclosed(x1-1, y1-1, x2+1, y2+1)[1]
 		except:
 			self._drag_data["window"] = None
 		self._drag_data["x"] = event.x
 		self._drag_data["y"] = event.y
+		print self._drag_data
 		
 
 	def OnTokenButtonRelease(self, event):
